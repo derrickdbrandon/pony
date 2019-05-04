105bcdd6446aa2b315e362ada70d92e0c0705ae8
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-05-04 19:03:36 -0400

    start building out src directory

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header.jsx';
+import MusicPlayer from './MusicPlayer.jsx';
+import Shows from './Shows.jsx';
+import Contact from './Contact.jsx';
+
+const App = () => {
+  return (
+    <Header/>
+    <MusicPlayer/>
+    <Shows/>
+    <Contact/>
+  );
+};
