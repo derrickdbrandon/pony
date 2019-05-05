396a59f901678c95357066812fb61176444c07f1
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-05-04 21:04:07 -0400

    add container links div

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,14 @@ const Header = () => {
   return (
     <div>
       <h1 className="title">DOT PONY</h1><br/>
-      <img height="40px" width="40px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/21497.png"/>
-      <img height="40px" width="40px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/soundcloud.png"/>
-      <img height="40px" width="40px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/instagram.png"/>
-      <img height="30px" width="36px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/twitter.png"/>
-      <img height="35px" width="35px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/220px-NTS_Radio_Logo_2018.png"/>
+      <div className="links">
+        <img className="link bandcamp" height="47px" width="47px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/bandcamp.png"/>
+        <img className="link soundcloud" height="36px" width="36px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/soundcloud.png"/>
+        <img className="link instagram" height="40px" width="40px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/instagram.png"/>
+        <img className="link twitter" height="30px" width="36px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/twitter.png"/>
+        <img className="link nts" height="33px" width="33px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/220px-NTS_Radio_Logo_2018.png"/>
+        <img className="link email" height="27px" width="37px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/email.png"/>
+      </div>
     </div>
   );
 };
