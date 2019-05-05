33bf2196660bbfa5fa20140f6047c122fc47ab90
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-05-04 20:22:06 -0400

    add site logos and title

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="title">DOT PONY</h1><br/>
+      <img height="40px" width="40px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/21497.png"/>
+      <img height="40px" width="40px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/soundcloud.png"/>
+      <img height="40px" width="40px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/instagram.png"/>
+      <img height="30px" width="36px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/twitter.png"/>
+      <img height="35px" width="35px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/220px-NTS_Radio_Logo_2018.png"/>
+    </div>
+  );
+};
+
+export default Header;
