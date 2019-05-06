2359525a2ed3aac276cc3dcffedd35300d5710e7
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-05-06 15:04:23 -0400

    created get songs method

--- db/index.js
+++ db/index.js
@@ -2,7 +2,23 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/dotpony', { useNewUrlParser: true });
 
-let songSchema = mongoose.Schema({
-  name: String,
+const songSchema = mongoose.Schema({
+  title: String,
   url: String,
 });
+
+const Song = mongoose.model('song', songSchema);
+
+const getSongs = (callback) => {
+  Song.find().exec((err, songs) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, songs);
+    }
+  });
+};
+
+module.exports = {
+  getSongs,
+};
