99b0dd496f119b40f66c6c4022cd6f09aa9af204
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-05-06 15:11:24 -0400

    retrieve songs from mongo database in get route

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const cors = require('cors');
 
 const bodyParser = require('body-parser');
 
+const Song = require('../db/index.js');
+
 const app = express();
 const PORT = 3000;
 
@@ -13,6 +15,16 @@ app.use(cors());
 app.use('/', express.static(path.join(__dirname, '/../dist')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+
+app.get('/songs', (req, res) => {
+  Song.getSongs((err, songs) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.send(songs);
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
