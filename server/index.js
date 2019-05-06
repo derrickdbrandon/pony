fe50b8ef3947f06ea1a978a3e14ad97154443474
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-05-05 21:50:54 -0400

    install cors and body parser for request and response formatting

--- server/index.js
+++ server/index.js
@@ -2,10 +2,17 @@ const express = require('express');
 
 const path = require('path');
 
+const cors = require('cors');
+
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, '/dist')));
+app.use(cors());
+app.use('/', express.static(path.join(__dirname, '/../dist')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
