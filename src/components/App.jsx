5a6e1dab9e0219f9dc9a84d35e247f34e3c158dd
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-05-06 22:37:52 -0400

    render shows component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import Header from './Header.jsx';
 import MusicPlayer from './MusicPlayer.jsx';
 import Shows from './Shows.jsx';
-import Contact from './Contact.jsx';
 
 const App = () => {
   return (
     <div>
       <Header/>
       <MusicPlayer/>
+      <Shows/>
     </div>
   );
 };
