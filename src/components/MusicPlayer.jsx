9307c7c598ecf8643bca1799a724b7d212db5033
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-05-06 22:39:13 -0400

    set up get request and initial song state

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,24 +1,55 @@
 import React from 'react';
+import Audio from './Audio.jsx';
+const axios = require('axios');
 
 class MusicPlayer extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      song: "https://s3.us-east-2.amazonaws.com/dotponyassets/expression.wav"
+      songs: [
+        {
+          _id: '5cd074750195920dafc3edee',
+          title: 'a (instrumental)',
+          url: 'https://s3.us-east-2.amazonaws.com/dotponyassets/01+a+(Instrumental).wav'
+        }
+      ],
+      currentSong: {
+        _id: '5cd074750195920dafc3edee',
+        title: 'a (instrumental)',
+        url: 'https://s3.us-east-2.amazonaws.com/dotponyassets/01+a+(Instrumental).wav'
+      }
     };
-  };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/songs').then(response => response.data).then((songs) => {
+      console.log(songs);
+      this.setState({
+        songs,
+      });
+    });
+  }
+
+  handleClick(e) {
+    let song = e.target.getAttribute('value');
+
+    this.setState({
+      currentSong: JSON.parse(song),
+    });
+  }
 
   render() {
     return (
       <div className="player">
-        <audio controls>
-        <source src={this.state.song} type="audio/wav"/>
-        </audio>
-        <p>a (instrumental)</p>
-        <p>work in progress</p>
+        <Audio title={this.state.currentSong.title} currentSong={this.state.currentSong} />
+        {this.state.songs.map((song) => {
+          return <p onClick={this.handleClick} className="songTitle" value={JSON.stringify(song)}>{song.title}</p>;
+        })}
       </div>
-    )
+    );
   };
 };
 
