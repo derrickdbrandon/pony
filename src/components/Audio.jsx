9069afd3cd1337c88331c70bcb835ea601874a86
Derrick Brandon <derrickdbrandon@gmail.com>
2019-06-22 01:19:17 -0400

    changed component to be a class component to update audio player on props change

--- src/components/Audio.jsx
+++ src/components/Audio.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 
-const Audio = ({ title, src }) => {
-  const { url } = src;
-  return (
-    <div>
-      <h1 className="songTitle">{title}</h1>
-      <audio controls>
-        <source src={url} type="audio/wav"/>
-      </audio>
-    </div>
-  );
-};
+class Audio extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: this.props.title,
+      src: this.props.src.url,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      title: nextProps.title,
+      src: nextProps.src.url,
+    }, function rerenderAudio() {
+      this.refs.audio.pause();
+      this.refs.audio.load();
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="songTitle">{this.state.title}</h1>
+        <audio controls ref="audio">
+          <source src={this.state.src} type="audio/wav"/>
+        </audio>
+      </div>
+    );
+  }
+}
 
 export default Audio;
