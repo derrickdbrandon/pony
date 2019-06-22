fc6d5919aaf982c3d4a7393c675bb2f7f53febc8
Derrick Brandon <derrickdbrandon@gmail.com>
2019-06-22 01:34:41 -0400

    add cassette link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         <a href="https://www.instagram.com/dotpony/" target="_blank"><img className="link instagram" height="40px" width="40px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/instagram.png"/></a>
         <a href="https://twitter.com/kimchipapito" target="_blank"><img className="link twitter" height="30px" width="36px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/twitter.png"/></a>
         <a href="https://www.nts.live/shows/dreamcast/episodes/dreamcast-5th-april-2019" target="_blank"><img className="link nts" height="33px" width="33px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/220px-NTS_Radio_Logo_2018.png"/></a>
-        <a href="mailto: dotpony94@gmail.com"><img className="link email" height="27px" width="37px" src="https://s3.us-east-2.amazonaws.com/dotponyassets/email.png"/></a>
+        <a href="https://ranchrecords.bigcartel.com/product/dot-pony-richard-phillip-smith-split-cass" target="_blank"><img className="link tape" height="50px" width="50px" src="https://dotponyassets.s3.us-east-2.amazonaws.com/cassette.png"/></a>
       </div>
     </div>
   );
