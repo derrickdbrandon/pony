bf66cdea1dc84f5a06a00dff5c103adb7bbee0fb
Derrick Brandon <derrickdbrandon@gmail.com>
2019-06-22 02:14:19 -0400

    add more songs to state

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -18,6 +18,21 @@ class MusicPlayer extends React.Component {
           title: 'a (instrumental)',
           url: 'https://s3.us-east-2.amazonaws.com/dotponyassets/01+a+(Instrumental).wav',
         },
+        {
+          _id: '5cd075f30195920dafc3edef',
+          title: 'last day',
+          url: 'https://s3.us-east-2.amazonaws.com/dotponyassets/04+last+day.wav',
+        },
+        {
+          _id: '5cd084160195920dafc3edf1',
+          title: 'room',
+          url: 'https://s3.us-east-2.amazonaws.com/dotponyassets/11+room.wav',
+        },
+        {
+          _id: '5cd0843f0195920dafc3edf2',
+          title: 'walk on by',
+          url: 'https://s3.us-east-2.amazonaws.com/dotponyassets/18+walk+on+by.wav',
+        },
       ],
       currentSong: {
         _id: '5cd083ee0195920dafc3edf0',
